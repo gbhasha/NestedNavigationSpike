69955bd8e431920deb7165aef2676bb2d1e8274b
Galeel Bhasha <gbhasha@gmail.com>
2019-09-15 21:52:16 +0530

    Updated with nested navigation

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-
-import {createAppContainer, SafeAreaView} from 'react-navigation';
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Image,
+  TouchableHighlight,
+  Dimensions,
+  YellowBox,
+} from 'react-native';
+import {NavigationActions, StackActions, SafeAreaView} from 'react-navigation';
+import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import {
+  createBottomTabNavigator,
   createMaterialTopTabNavigator,
   MaterialTopTabBar,
 } from 'react-navigation-tabs';
-import {YellowBox} from 'react-native';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     YellowBox.ignoreWarnings([
       'Warning: componentWillMount is deprecated',
@@ -28,25 +31,105 @@ class App extends React.Component {
 }
 export default App;
 
-const ScreenDetails = ({children}) => (
-  <SafeAreaView style={styles.container}>{children}</SafeAreaView>
+const Detail = props => (
+  <SafeAreaView style={{flex: 1}}>
+    <View style={styles.container}>{props.children}</View>
+  </SafeAreaView>
 );
 
-const Nav1Screen = () => (
-  <ScreenDetails>
-    <Text>Nav1 Screen</Text>
-  </ScreenDetails>
+const Tab1Screen = () => (
+  <View style={styles.container}>
+    <Text style={{color: 'brown', fontSize: 25}}>Welcome Home</Text>
+  </View>
 );
-const Nav2Screen = () => (
-  <ScreenDetails>
-    <Text>Nav1 Screen</Text>
-  </ScreenDetails>
+
+const Tab3Screen = () => (
+  <Detail>
+    <Text>Tab3 Screen</Text>
+  </Detail>
 );
-const Nav3Screen = () => (
-  <ScreenDetails>
-    <Text>Nav1 Screen</Text>
-  </ScreenDetails>
+
+const Tab2DetailScreen = ({navigation}) => (
+  <Detail>
+    <Text>Tab2 > Nav2 > Nav1 > Detail > Detail Screen</Text>
+    <Button
+      title="Go to Next Screen"
+      onPress={() => navigation.navigate('Detail3')}
+    />
+  </Detail>
 );
+
+const Tab2Nav2FinalDetailsScreen = ({navigation}) => (
+  <Detail>
+    <View style={{...styles.container, flex: 0.8}}>
+      <Text style={{color: 'green', fontSize: 40}}> Success !!!! </Text>
+    </View>
+    <View style={{...styles.container, flex: 0.2}}>
+      <TouchableHighlight
+        style={styles.primaryBtn}
+        onPress={() => {
+          const resetAction = StackActions.reset({
+            index: 0,
+            actions: [
+              NavigationActions.navigate({
+                routeName: 'DashboardTabNavigator',
+              }),
+            ],
+          });
+          navigation.dispatch(resetAction);
+        }}>
+        <Text style={{color: 'white', fontWeight: 'bold', textAlign: 'center'}}>
+          Goto Home
+        </Text>
+      </TouchableHighlight>
+    </View>
+  </Detail>
+);
+
+const Tab2Nav2Nav1DetailScreen = ({navigation}) => (
+  <Detail>
+    <Text>Tab2 > Nav2 > Nav1 > DetailScreen</Text>
+    <Button
+      title="Goto Next Screen"
+      onPress={() => navigation.navigate('Detail')}
+    />
+  </Detail>
+);
+
+const Tab2Nav2Nav2DetailScreen = () => (
+  <Detail>
+    <Text>Tab2 > Nav2 > Nav2 > DetailScreen</Text>
+  </Detail>
+);
+
+const Tab2Nav1Screen = () => (
+  <Detail>
+    <Text>Tab2 > Nav1 > DetailScreen</Text>
+  </Detail>
+);
+
+const Tab2Nav2Screen = ({navigation}) => (
+  <Detail>
+    <Text>Tab2 > Nav2 > DetailScreen</Text>
+    <Button
+      title="Goto Next Screen"
+      onPress={() => navigation.navigate('Tab2Nav1Screen')}
+    />
+  </Detail>
+);
+
+const Tab2Nav3Screen = () => (
+  <Detail>
+    <Text>Tab2 > Nav3 > DetailScreen</Text>
+  </Detail>
+);
+
+const Tab2Nav4Screen = () => (
+  <Detail>
+    <Text>Tab2 > Nav4 > DetailScreen</Text>
+  </Detail>
+);
+
 function SafeAreaMaterialTopTabBar(props) {
   return (
     <SafeAreaView>
@@ -54,23 +137,173 @@ function SafeAreaMaterialTopTabBar(props) {
     </SafeAreaView>
   );
 }
-const TopNavScreens = createMaterialTopTabNavigator(
+
+const Tab2Nav2DetailsTopNavScreens = createMaterialTopTabNavigator(
   {
-    Nav1: Nav1Screen,
-    Nav2: Nav2Screen,
-    Nav3: Nav3Screen,
+    nav1: {screen: Tab2Nav2Nav1DetailScreen},
+    nav2: {screen: Tab2Nav2Nav2DetailScreen},
   },
   {
-    tabBarComponent: SafeAreaMaterialTopTabBar,
+    initialLayout: {
+      height: 0,
+      width: Dimensions.get('window').width,
+    },
+    swipeEnabled: true,
+    tabBarOptions: {
+      activeTintColor: 'black',
+      inactiveTintColor: 'gray',
+      labelStyle: {
+        textAlign: 'center',
+      },
+      indicatorStyle: {
+        borderBottomColor: 'brown',
+        borderBottomWidth: 2,
+      },
+      style: {
+        backgroundColor: 'white',
+        borderColor: 'orange',
+      },
+    },
   },
 );
 
-const DashboardStack = createStackNavigator(
+const TopTabScreens = createMaterialTopTabNavigator(
   {
-    Dashboard: TopNavScreens,
+    nav1: {screen: Tab2Nav1Screen},
+    nav2: {screen: Tab2Nav2Screen},
+    nav3: {screen: Tab2Nav3Screen},
+    nav4: {screen: Tab2Nav4Screen},
+  },
+
+  {
+    initialRouteName: 'nav2',
+    swipeEnabled: true,
+    tabBarComponent: SafeAreaMaterialTopTabBar,
+    tabBarOptions: {
+      activeTintColor: 'black',
+      inactiveTintColor: 'gray',
+      labelStyle: {
+        textAlign: 'center',
+      },
+      indicatorStyle: {
+        borderBottomColor: 'brown',
+        borderBottomWidth: 2,
+      },
+      style: {
+        backgroundColor: 'white',
+        borderColor: 'transparent',
+      },
+    },
+  },
+);
+
+const HeaderLeft = ({navigation}) => (
+  <TouchableOpacity
+    onPress={() => {
+      navigation.goBack();
+    }}>
+    <Image
+      source={require('./backImage.png')}
+      style={{height: 26, width: 26, marginLeft: 16}}
+    />
+  </TouchableOpacity>
+);
+
+const Tab2Stack = createStackNavigator(
+  {
+    TabScreen: {
+      screen: TopTabScreens,
+      navigationOptions: {
+        header: null,
+      },
+    },
+    Tab2Nav1Screen: {
+      screen: Tab2Nav2DetailsTopNavScreens,
+      navigationOptions: ({navigation}) => {
+        return {
+          title: 'Title',
+          headerLeft: <HeaderLeft navigation={navigation} />,
+        };
+      },
+    },
+    Detail: {
+      screen: Tab2DetailScreen,
+      navigationOptions: ({navigation}) => {
+        return {
+          headerLeft: <HeaderLeft navigation={navigation} />,
+        };
+      },
+    },
+    Detail3: {
+      screen: Tab2Nav2FinalDetailsScreen,
+      navigationOptions: ({navigation}) => {
+        return {
+          header: null,
+          tabBarVisible: false,
+        };
+      },
+    },
   },
   {
-    defaultNavigationOptions: () => {
+    navigationOptions: ({navigation}) => {
+      return {
+        // Hide Bottom Navigation in Nav2 screens
+        tabBarVisible: !navigation.state.index > 0,
+      };
+    },
+  },
+);
+
+// // Hide Bottom Navigation in Nav2 screens
+// Tab2Stack.navigationOptions = ({navigation}) => {
+//   let tabBarVisible = true;
+//   if (navigation.state.index > 0) {
+//     tabBarVisible = false;
+//   }
+//   return {
+//     tabBarVisible
+//   };
+// };
+
+const Tab1Stack = createStackNavigator({
+  Tab1Screen: {
+    screen: Tab1Screen,
+    navigationOptions: ({navigation}) => {
+      return {
+        header: null,
+      };
+    },
+  },
+});
+
+const Tab3Stack = createStackNavigator({
+  Tab3Screen: {
+    screen: Tab3Screen,
+    navigationOptions: ({navigation}) => {
+      return {
+        header: null,
+      };
+    },
+  },
+});
+
+const DashboardTabNavigator = createBottomTabNavigator(
+  {
+    Tab1Stack: Tab1Stack,
+    Tab2Stack: Tab2Stack,
+    Tab3Stack: Tab3Stack,
+  },
+  {
+    initialRouteName: 'Tab2Stack',
+  },
+);
+
+const DashboardStackNavigator = createStackNavigator(
+  {
+    DashboardTabNavigator: DashboardTabNavigator,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => {
       return {
         header: null,
       };
@@ -78,11 +311,21 @@ const DashboardStack = createStackNavigator(
   },
 );
 
-const AppContainer = createAppContainer(DashboardStack);
+const AppContainer = createAppContainer(DashboardStackNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  primaryBtn: {
+    height: 40,
+    width: 160,
+    borderRadius: 20,
+    backgroundColor: 'rgb(136,15,30)',
+    marginLeft: 50,
+    marginRight: 50,
     alignItems: 'center',
     justifyContent: 'center',
   },
